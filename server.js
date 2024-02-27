9a9b32bee1da7af26879dabdd8f5869b0242d7c9
wiraphat <132466647+wiraphatys@users.noreply.github.com>
2024-02-27 13:19:08 +0700

    initialize project structure.

---
+++ server.js
@@ -0,0 +1,32 @@
+// express
+const express = require('express');
+const app = express();
+app.use(express.json())     // body-parser
+
+// env
+const dotenv = require('dotenv');
+dotenv.config({ path: "./config/config.env" });
+
+// port & mode
+const PORT = process.env.PORT || 3000;
+const MODE = process.env.NODE_ENV;
+
+// cookie-parser
+const cookieParser = require('cookie-parser')
+app.use(cookieParser)
+
+// router
+
+
+// listening
+const server = app.listen(PORT, () => {
+    console.log(`This server running in ${MODE} mode on a port ${PORT}`)
+})
+
+// handle error
+process.on('unhandledRejection', (err) => {
+    console.log(`Error: ${err.message}`);
+    server.close(() => {
+        process.exit(1);
+    })
+})
