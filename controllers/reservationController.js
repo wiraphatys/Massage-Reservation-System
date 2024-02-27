dad61c51b1de49e4be4f465f17fe6e7adaaa5b62
wiraphat <132466647+wiraphatys@users.noreply.github.com>
2024-02-27 22:27:28 +0700

    Added API logic for Massage module

--- controllers/reservationController.js
+++ controllers/reservationController.js
@@ -4,33 +4,53 @@ const Reservation = require('../models/ReservationModel');
 // @route   GET /api/reservations/
 // @access  Private
 exports.getReservations = async (req, res, next) => {
+    try {
 
+    } catch (err) {
+
+    }
 }
 
 // @desc    Get single reservation
 // @route   GET /api/reservations/:id
 // @access  Private
 exports.getReservationByID = async (req, res, next) => {
+    try {
 
+    } catch (err) {
+
+    }
 }
 
 // @desc    Add a new reservation
 // @route   POST /api/reservations/
 // @access  Private
 exports.createReservation = async (req, res, next) => {
+    try {
 
+    } catch (err) {
+
+    }
 }
 
 // @desc    Update a reservation
 // @route   PUT /api/reservations/:id
 // @access  Private
 exports.updateReservation = async (req, res, next) => {
+    try {
 
+    } catch (err) {
+
+    }
 }
 
 // @desc    Delete a reservation
 // @route   DELETE /api/reservations/:id
 // @access  Private
 exports.deleteReservation = async (req, res, next) => {
-    
+    try {
+
+    } catch (err) {
+
+    }
 }
