5734b03dd222b660622f8b1742de67295f7fc746
wiraphat <132466647+wiraphatys@users.noreply.github.com>
2024-02-27 14:27:44 +0700

    Added structure endpoint API of Massage module

---
+++ controllers/massageController.js
@@ -0,0 +1,36 @@
+const Massage = require('../models/MassageModel');
+
+// @desc    get all massages
+// @route   GET /api/v1/massages/
+// @access  Public
+exports.getMassages = async (req, res, next) => {
+
+}
+
+// @desc    get single massage
+// @route   GET /api/v1/massages/:id
+// @access  Public
+exports.getMassageByID = async (req, res, next) => {
+
+}
+
+// @desc    create a new massage
+// @route   POST /api/v1/massages/
+// @access  Private
+exports.createMassage = async (req, res, next) => {
+
+}
+
+// @desc    update a massage by id
+// @route   PUT /api/v1/massages/:id
+// @access  Private
+exports.updateMassage = async (req, res, next) => {
+
+}
+
+// @desc    delete a massage by id
+// @route   DELETE /api/v1/massages/:id
+// @access  Private
+exports.deleteMassage = async (req, res, next) => {
+
+}
