8d5e8e155d8a9c848b212c99b3ca585dfcdde2f3
wiraphat <132466647+wiraphatys@users.noreply.github.com>
2024-02-27 21:18:02 +0700

    Updated package for API Security

--- server.js
+++ server.js
@@ -1,28 +1,43 @@
-// express
-const express = require('express');
+// Require package
+const express = require("express");
 const app = express();
-app.use(express.json());     // body-parser
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const {xss} = require("express-xss-sanitizer");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const hpp = require("hpp");
+const cors = require("cors");
 
-// env
-const dotenv = require('dotenv');
+// Import file
+const connectDB = require("./config/db");
+
+// Load ENV
 dotenv.config({ path: "./config/config.env" });
 
-// port & mode
+// Define variable
 const PORT = process.env.PORT || 3000;
-const MODE = process.env.NODE_ENV;
+const MODE = process.env.MODE;
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000,    // 10 minute
+    max: 100
+})
 
-// cookie-parser
-const cookieParser = require('cookie-parser');
-app.use(cookieParser);
-
-// connectDB
-const connectDB = require('./config/db');
+// Connect database
 connectDB();
 
-// router
-app.use("/api/massages", require("./routes/massageRouter"));
-app.use("/api/auth", require("./routes/authRouter"));
-app.use("/api/reservations", require("./routes/reservationRouter"));
+// Use
+app.use(express.json());        // body-parser
+app.use(cookieParser);          // cookie-parser
+app.use(mongoSanitize());       // express-mongo-sanitize
+app.use(helmet());              // helmet
+app.use(xss());                 // express-xss-sanitizer
+app.use(limiter);               // express-rate-limit
+app.use(hpp());                 // hpp
+app.use(cors());                // cors
+
+// Route
 
 // listening
 const server = app.listen(PORT, () => {
