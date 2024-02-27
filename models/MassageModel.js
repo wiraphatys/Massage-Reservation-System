b08c612e962c4e743566772eef11cebfa3974453
vineyard <vineyard.achi@gmail.com>
2024-02-27 23:28:22 +0700

    Updated MassageModel

--- models/MassageModel.js
+++ models/MassageModel.js
@@ -3,26 +3,26 @@ const mongoose = require('mongoose')
 const MassageSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: [true, "Please add a name."],
+        required: [true, 'Please add a name'],
         unique: true,
         trim: true,
-        maxlength: [50, "name cannot be more than 50 characters."]
+        maxlength: [50, 'name cannot be more than 50 characters']
     },
     address: {
         type: String,
-        required: [true, "Please add an address."]
+        required: [true, 'Please add an address']
     },
     district: {
         type: String,
-        required: [true, "Please add a district."]
+        required: [true, 'Please add a district']
     },
     province: {
         type: String,
-        required: [true, "Please add a province."]
+        required: [true, 'Please add a province']
     },
     tel: {
         type: String,
-        required: [true, "Please add a tel"]
+        required: [true, 'Please add a tel']
     }
 });
 
